144972dfe29e5f519377d9ce718cc5fddb399a6c
Remco Haszing <remcohaszing@gmail.com>
2019-01-03 13:43:51 +0100

    Replace mocha and chai with jest
    Chai doesn’t support comparison of Symbol properties. This is needed for
    the filter tests.

---
+++ __tests__/test.js
@@ -0,0 +1,48 @@
+const parser = require("../index");
+
+const sequelize = jest.requireMock("sequelize");
+
+describe("#odataParser", () => {
+  it("should parse top", () => {
+    const result = parser("$top=10", sequelize);
+    expect(result).toStrictEqual({
+      limit: 10
+    });
+  });
+
+  it("should parse skip", () => {
+    const result = parser("$skip=5", sequelize);
+    expect(result).toStrictEqual({
+      offset: 5
+    });
+  });
+
+  it("should parse top skip", () => {
+    const result = parser("$top=10&$skip=5", sequelize);
+    expect(result).toStrictEqual({
+      limit: 10,
+      offset: 5
+    });
+  });
+
+  it("should parse select", () => {
+    const result = parser("$select=foo", sequelize);
+    expect(result).toStrictEqual({
+      attributes: ["foo"]
+    });
+  });
+
+  it("should parse orderby", () => {
+    const result = parser("$orderby=name desc", sequelize);
+    expect(result).toStrictEqual({
+      order: [["name", "DESC"]]
+    });
+  });
+
+  it("should parse orderby with multiple columns", () => {
+    const result = parser("$orderby=name desc,ranking", sequelize);
+    expect(result).toStrictEqual({
+      order: [["name", "DESC"], ["ranking", "ASC"]]
+    });
+  });
+});
