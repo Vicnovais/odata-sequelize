d92a382c37ddff533733650ca9a75ab806a5c504
Remco Haszing <remcohaszing@gmail.com>
2020-05-13 20:53:05 +0200

    Update index.d.ts
    Co-authored-by: Wessel Kuipers <WesselKuipers@users.noreply.github.com>

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import type { QueryOptions, Sequelize } from 'sequelize';
 
 /**
- * This library is intended to take an OData query string as a parameter and transform it on a
+ * This library is intended to take an OData query string as a parameter and transform it into a
  * sequelize-compliant query.
  */
 declare function parseOData(query: string, sequelize: Sequelize): QueryOptions;
